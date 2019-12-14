1b0307118e8e63f45a1d7bd811e6a7d76dba022e
shirayaae <44359282+shirayaae@users.noreply.github.com>
2019-12-14 13:36:18 +0800

    Update rumor.js

--- commands/Misc/rumor.js
+++ commands/Misc/rumor.js
@@ -20,11 +20,11 @@ module.exports = class RumorsCommand extends Command {
       message.channel.fetchMessages({limit: 100}).then(messages => {
         let members = new Map();
         messages.map(m => { if (m.member) {
-          members.set(m.member.nickname, m.member);
+          members.set(m.member.id, m.member);
         }});
-        if (members.size >= 3) members.delete(this.main.client.user.nickname);
+        if (members.size >= 3) members.delete(this.main.client.user.id);
         let player1 = this.main.util.ARand(Array.from(members.values()));
-        members.delete(player1.nickname);
+        members.delete(player1.id);
         let player2 = this.main.util.ARand(Array.from(members.values()));
         message.channel.send(this.main.util.ARand(this.rumors).replace('[1]', player1.nickname).replace('[2]', player2.nickname));
       })
