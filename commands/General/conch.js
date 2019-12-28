d039d53c1fa41f1d6a8c005d5290effec28e3580
shirayaae <44359282+shirayaae@users.noreply.github.com>
2019-12-28 02:03:44 -0800

    Add files via upload

---
+++ commands/General/conch.js
@@ -0,0 +1,38 @@
+const Command = require('../../main/command');
+
+module.exports = class ConchCommand extends Command {
+  constructor(main) {
+    super(main, {
+      name: "Conch",
+      category: "General",
+      help: "Ask the Magic Conch™",
+      args: [
+        {
+          name: "Text",
+          desc: "ALL HAIL THE MAGIC CONCH™ "
+        }
+      ], 
+      caseSensitive: false
+    })
+  }
+   run(message, args, prefix) {
+	const responses = [
+		"Yes",
+		"No",
+		"Maybe someday",
+		"I don't think so",
+		"Try asking again",
+		"Sure",
+		"Probably not",
+		" It is certain",
+		"Outlook not so good",
+		"Outlook good",
+		"Doubtful",
+		"Without a doubt",
+		"Don't"
+	]; 
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    if (args) message.channel.send(responses[randomIndex]);
+    else message.channel.send("Please ask a question");
+  }
+}
