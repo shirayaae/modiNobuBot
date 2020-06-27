b7e590f9c4e2da5ab06099349bf61ad6a09f333a
shirayaae <44359282+shirayaae@users.noreply.github.com>
2020-06-28 00:33:56 +0800

    Add files via upload

--- commands/Images/coffee.js
+++ commands/Images/coffee.js
@@ -19,10 +19,10 @@ module.exports = class IWCommand extends Command {
   run(message, args, prefix) {
     args = message.mentions.users.first() || message.author;
     message.guild.fetchMember(args).then(i => {
+      let avatar = i.user.avatarURL.split('?')[0];
       if (this.main.util.rand(0, 1)) {
           message.channel.send('', {file: {attachment: "https://i.imgur.com/K3KfZ91.jpg", name: 'Dr Roman'}});
         });
       } else message.channel.send('', {file: {attachment: "https://i.imgur.com/HFs9ORR.jpg", name: 'Da Vinci'}});
-    ;
   }
 }
