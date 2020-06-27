432873aac7f588ec7aa0b8d8376fc64c8e50864f
shirayaae <44359282+shirayaae@users.noreply.github.com>
2020-06-28 00:47:02 +0800

    Update coffee.js

--- commands/Images/coffee.js
+++ commands/Images/coffee.js
@@ -20,5 +20,5 @@ module.exports = class CoffeeComand extends Command {
       if (this.main.util.rand(0, 1)) {
           message.channel.send('', {file: {attachment: "https://i.imgur.com/K3KfZ91.jpg", name: 'Dr Roman'}});
       } else message.channel.send('', {file: {attachment: "https://i.imgur.com/HFs9ORR.jpg", name: 'Da Vinci'}});
-   };
+   }
 }
