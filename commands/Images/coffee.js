9e778ef3a58437f089074c0670ba9cdd9f0a5119
shirayaae <44359282+shirayaae@users.noreply.github.com>
2020-06-28 00:44:10 +0800

    Add files via upload

--- commands/Images/coffee.js
+++ commands/Images/coffee.js
@@ -2,7 +2,7 @@ const Command = require('../../main/command');
 const Canvas = require('canvas');
 const snek = require('snekfetch');
 
-module.exports = class IWCommand extends Command {
+module.exports = class CoffeeComand extends Command {
   constructor(main) {
     super(main, {
       name: "coffee",
@@ -18,9 +18,8 @@ module.exports = class IWCommand extends Command {
   }
   run(message, args, prefix) {
     message.guild.fetchMember(args).then(i => {
-      let avatar = i.user.avatarURL.split('?')[0];
       if (this.main.util.rand(0, 1)) {
-        this.main.util.dust(avatar).then(attachment => {
+        {
           message.channel.send('', {file: {attachment: "https://i.imgur.com/K3KfZ91.jpg", name: 'Dr Roman'}});
         });
       } else message.channel.send('', {file: {attachment: "https://i.imgur.com/HFs9ORR.jpg", name: 'Da Vinci'}});
